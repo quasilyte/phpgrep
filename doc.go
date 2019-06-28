f1a100c54d2ac890f54c201d07c4a3f1ac65066e
Iskander Sharipov <quasilyte@gmail.com>
2019-06-28 14:13:01 +0300

    add code layout stubs
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Package phpgrep is a library for searching PHP code
+// using syntax trees.
+//
+// Inspired by mvdan/gogrep.
+//
+// TODO(quasilyte): add the actual overview.
+package phpgrep
---
+++ phpgrep.go
@@ -0,0 +1,3 @@
+package phpgrep
+
+// TODO(quasilyte): implement me.
