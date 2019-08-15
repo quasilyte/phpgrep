4c71cc635bd930fcbc284e77421b504a8afa7614
Iskander Sharipov <quasilyte@gmail.com>
2019-08-15 23:37:39 +0300

    add benchmarks
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- matcher_test.go
+++ matcher_test.go
@@ -9,7 +9,7 @@ type matcherTest struct {
 	input   string
 }
 
-func mustCompile(t *testing.T, c *Compiler, code string) *Matcher {
+func mustCompile(t testing.TB, c *Compiler, code string) *Matcher {
 	matcher, err := c.Compile([]byte(code))
 	if err != nil {
 		t.Fatalf("pattern compilation error:\ntext: %q\nerr: %v", code, err)
@@ -465,4 +465,155 @@ func TestMatchNegative(t *testing.T) {
 		{`${"const"}`, `$x->y`},
 		{`$_ = ${"const"}`, `$x = MyClass::$var`},
 	})
+}
+
+func BenchmarkFind(b *testing.B) {
+	input := []byte(`<?php
+
+use N\{ClassName,
+  AnotherClassName,
+  OneMoreClassName};
+
+namespace A {
+  function foo() {
+    return 0;
+  }
+
+  function bar($x,
+    $y, int $z = 1) {
+    $x = 0;
+// $x = 1
+    do {
+      $y += 1;
+    } while ($y < 10);
+    if (true)
+      $x = 10;
+    elseif ($y < 10)
+      $x = 5;
+    elseif (true)
+      $x = 5;
+    for ($i = 0; $i < 10; $i++)
+      $yy = $x > 2 ? 1 : 2;
+    while (true)
+      $x = 0;
+    do {
+      $x += 1;
+    } while (true);
+    foreach (["a" => 0, "b" => 1,
+              "c" => 2] as $e1) {
+      echo $e1;
+    }
+    $count = 10;
+    $x     = ["x", "y",
+      [1 => "abc",
+       2 => "def", 3 => "ghi"]];
+    $zz    = [0.1, 0.2,
+      0.3, 0.4];
+    $x     = [
+      0   => "zero",
+      123 => "one two three",
+      25  => "two five",
+    ];
+    bar(0, bar(1,
+      "b"));
+  }
+
+  abstract class Foo extends FooBaseClass implements Bar1, Bar2, Bar3 {
+
+    var $numbers = ["one", "two", "three", "four", "five", "six"];
+    var $v = 0;
+    public $path = "root";
+
+    const FIRST  = 'first';
+    const SECOND = 0;
+    const Z      = -1;
+
+    function bar($v,
+      $w = "a") {
+      $y      = $w;
+      $result = foo("arg1",
+        "arg2",
+        10);
+      switch ($v) {
+        case 0:
+          return 1;
+        case 1:
+          echo '1';
+          break;
+        case 2:
+          break;
+        default:
+          $result = 10;
+      }
+      return $result;
+    }
+
+    public static function fOne($argA, $argB, $argC, $argD, $argE, $argF, $argG, $argH) {
+      $x = $argA + $argB + $argC + $argD + $argE + $argF + $argG + $argH;
+      list($field1, $field2, $field3, $filed4, $field5, $field6) = explode(",", $x);
+      fTwo($argA, $argB, $argC, fThree($argD, $argE, $argF, $argG, $argH));
+      $z      = $argA == "Some string" ? "yes" : "no";
+      $colors = ["red", "green", "blue", "black", "white", "gray"];
+      $count  = count($colors);
+      for ($i = 0; $i < $count; $i++) {
+        $colorString = $colors[$i];
+      }
+    }
+
+    function fTwo($strA, $strB, $strC, $strD) {
+      if ($strA == "one" || $strB == "two" || $strC == "three") {
+        return $strA + $strB + $strC;
+      }
+      $x = $foo->one("a", "b")->two("c", "d", "e")->three("fg")->four();
+      $y = a()->b()->c();
+      return $strD;
+    }
+
+    function fThree($strA, $strB, $strC, $strD, $strE) {
+      try {
+      } catch (Exception $e) {
+        foo();
+      } finally {
+        // do something
+      }
+      return $strA + $strB + $strC + $strD + $strE;
+    }
+
+    protected abstract function fFour();
+
+  }
+}
+
+function f() {}
+
+$_ = f(1 + 2 + 3 + 4 + 5);
+$_ = f(f() + f() + f() + f() + f());
+`)
+
+	var c Compiler
+
+	b.Run("positive/with-1-named", func(b *testing.B) {
+		matcher := mustCompile(b, &c, `$x`)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			matcher.Find(input, func(m *MatchData) bool { return true })
+		}
+	})
+
+	b.Run("positive/with-5-named", func(b *testing.B) {
+		matcher := mustCompile(b, &c, `$x1 + $x2 + $x3 + $x4 + $x5`)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			matcher.Find(input, func(m *MatchData) bool { return true })
+		}
+	})
+
+	b.Run("negative/with-0-named", func(b *testing.B) {
+		matcher := mustCompile(b, &c, `1 + 7 - 103`)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			matcher.Find(input, func(m *MatchData) bool { return true })
+		}
+	})
+
 }
