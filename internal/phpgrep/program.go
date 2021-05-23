dab91edc344ead2704289cab2a15165d189d0e9d
Iskander Sharipov <quasilyte@gmail.com>
2021-05-23 07:59:44 +0300

    internal/phpgrep: add more colors and support env vars

--- internal/phpgrep/program.go
+++ internal/phpgrep/program.go
@@ -325,18 +325,30 @@ func mustColorizeText(s, color string) string {
 	return result
 }
 
+var ansiColorMap = map[string]string{
+	"dark-red": "31m",
+	"red":      "31;1m",
+
+	"dark-green": "32m",
+	"green":      "32;1m",
+
+	"dark-blue": "34m",
+	"blue":      "34;1m",
+
+	"dark-magenta": "35m",
+	"magenta":      "35;1m",
+}
+
 func colorizeText(s, color string) (string, error) {
 	switch color {
-	case "":
+	case "", "white":
 		return s, nil
-	case "red":
-		return "\033[31m" + s + "\033[0m", nil
-	case "green":
-		return "\033[32m" + s + "\033[0m", nil
-	case "purple":
-		return "\033[35m" + s + "\033[0m", nil
 	default:
-		return "", fmt.Errorf("unsupported color: %s", color)
+		escape, ok := ansiColorMap[color]
+		if !ok {
+			return "", fmt.Errorf("unsupported color: %s", color)
+		}
+		return "\033[" + escape + s + "\033[0m", nil
 	}
 }
 
